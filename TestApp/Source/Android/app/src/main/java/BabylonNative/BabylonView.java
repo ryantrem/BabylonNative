b59657be1afe3f5d400be987ffd479c09ca9d071
Ryan Tremblay <ryantrem@microsoft.com>
2020-01-17 14:14:20 -0800

    Testing out using TextureView

--- TestApp/Source/Android/app/src/main/java/BabylonNative/BabylonView.java
+++ TestApp/Source/Android/app/src/main/java/BabylonNative/BabylonView.java
@@ -1,16 +1,25 @@
 package BabylonNative;
 
 import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
+import android.view.Surface;
 import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
+
+import android.opengl.GLES20;
+import android.opengl.GLES11Ext;
+import android.opengl.GLU;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
-public class BabylonView extends SurfaceView implements SurfaceHolder.Callback2, View.OnTouchListener {
+public class BabylonView extends TextureView implements TextureView.SurfaceTextureListener, View.OnTouchListener {
     private static final String TAG = "BabylonView";
     private boolean mViewReady = false;
     private ViewDelegate mViewDelegate;
@@ -19,11 +28,58 @@ public class BabylonView extends SurfaceView implements SurfaceHolder.Callback2,
         super(context);
         init(viewDelegate);
         BabylonNative.Wrapper.initEngine(context.getResources().getAssets());
+
+        /*Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable()
+        {
+            public void run() {
+                int[] textures = new int[1];
+                GLES20.glGenTextures(1, textures, 0);
+                int error = GLES20.glGetError();
+                Log.d("mytag", "TEXTURE ERROR " + GLU.gluErrorString(error));
+                Log.d("mytag", "TEXTURE ID " + textures[0]);
+
+                GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0]);
+                GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+                GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+                GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+                GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+                SurfaceTexture surfaceTexture = new SurfaceTexture(textures[0], false);
+                setSurfaceTexture(surfaceTexture);
+
+                BabylonNative.Wrapper.surfaceCreated(new Surface(surfaceTexture));
+                if (!mViewReady) {
+                    mViewDelegate.onViewReady();
+                    mViewReady = true;
+                }
+            }
+        });*/
+
+        int[] textures = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+        int error = GLES20.glGetError();
+        Log.d("mytag", "TEXTURE ERROR " + GLU.gluErrorString(error));
+        Log.d("mytag", "TEXTURE ID " + textures[0]);
+
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0]);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+        SurfaceTexture surfaceTexture = new SurfaceTexture(textures[0], false);
+        setSurfaceTexture(surfaceTexture);
+
+        BabylonNative.Wrapper.surfaceCreated(new Surface(surfaceTexture));
+        /*if (!mViewReady) {
+            mViewDelegate.onViewReady();
+            mViewReady = true;
+        }*/
     }
 
     private void init(ViewDelegate viewDelegate) {
-        SurfaceHolder holder = getHolder();
-        holder.addCallback(this);
+        setSurfaceTextureListener(this);
         setOnTouchListener(this);
         mViewDelegate = viewDelegate;
     }
@@ -49,8 +105,24 @@ public class BabylonView extends SurfaceView implements SurfaceHolder.Callback2,
      * This method is part of the SurfaceHolder.Callback interface, and is
      * not normally called or subclassed by clients of BabylonView.
      */
-    public void surfaceCreated(SurfaceHolder holder) {
-        BabylonNative.Wrapper.surfaceCreated(getHolder().getSurface());
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+       /* int[] textures = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+        int error = GLES20.glGetError();
+        Log.d("mytag", "TEXTURE ERROR " + GLU.gluErrorString(error));
+        Log.d("mytag", "TEXTURE ID " + textures[0]);
+
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0]);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+        surface = new SurfaceTexture(textures[0], false);
+        //surfaceTexture.detachFromGLContext();
+        setSurfaceTexture(surface);*/
+
+        BabylonNative.Wrapper.surfaceCreated(new Surface(surface));
         if (!this.mViewReady) {
             mViewDelegate.onViewReady();
             mViewReady = true;
@@ -61,15 +133,20 @@ public class BabylonView extends SurfaceView implements SurfaceHolder.Callback2,
      * This method is part of the SurfaceHolder.Callback interface, and is
      * not normally called or subclassed by clients of BabylonView.
      */
-    public void surfaceDestroyed(SurfaceHolder holder) {
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return true;
     }
 
     /**
      * This method is part of the SurfaceHolder.Callback interface, and is
      * not normally called or subclassed by clients of BabylonView.
      */
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        BabylonNative.Wrapper.surfaceChanged(w, h, getHolder().getSurface());
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int w, int h) {
+        BabylonNative.Wrapper.surfaceChanged(w, h, new Surface(surface));
+    }
+
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        BabylonNative.Wrapper.surfaceChanged(getWidth(), getHeight(), new Surface(surface));
     }
 
     public interface ViewDelegate {
@@ -98,16 +175,5 @@ public class BabylonView extends SurfaceView implements SurfaceHolder.Callback2,
     @Override
     protected void finalize() throws Throwable {
         BabylonNative.Wrapper.finishEngine();
-    }
-
-    /**
-     * This method is part of the SurfaceHolder.Callback2 interface, and is
-     * not normally called or subclassed by clients of BabylonView.
-     */
-    @Deprecated
-    @Override
-    public void surfaceRedrawNeeded(SurfaceHolder holder) {
-        // Since we are part of the framework we know only surfaceRedrawNeededAsync
-        // will be called.
     }
 }
